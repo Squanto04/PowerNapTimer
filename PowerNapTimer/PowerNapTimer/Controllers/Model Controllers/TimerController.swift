92ff5732ba202b1f0eff202c45ba31b170abc678
Jordan Lamb <jdawg2329@gmail.com>
2019-09-24 10:45:02 -0600

    Adds TimerController file and methods

--- PowerNapTimer/PowerNapTimer/Controllers/Model Controllers/TimerController.swift
+++ PowerNapTimer/PowerNapTimer/Controllers/Model Controllers/TimerController.swift
@@ -7,3 +7,53 @@
 //
 
 import Foundation
+
+protocol TimerDelegate: class {
+    func timerSecTicked()
+    func timerCompleted()
+    func timerStopped()
+}
+
+class TimerController {
+    
+    //Source of Truth - Built in classes
+    var timer: Timer?
+    var timeRemaining: TimeInterval?
+    
+    weak var delegate: TimerDelegate?
+    
+    var isOn: Bool {
+            return timeRemaining != nil ? true : false
+        }
+    
+    func secondTick() {
+        guard let timeRemaining = timeRemaining else {return}
+        if timeRemaining > 0 {
+            self.timeRemaining = timeRemaining - 1
+            delegate?.timerSecTicked()
+        } else {
+            timer?.invalidate()
+            self.timeRemaining = nil
+            delegate?.timerCompleted()
+        }
+    }
+    
+    func startTimer(_ time: TimeInterval) {
+        if isOn == false {
+            timeRemaining = time
+            DispatchQueue.main.async {
+//                secondTick()
+                self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { (_) in self.secondTick()
+                })
+            }
+        }
+    }
+    
+    func stopTimer() {
+        if isOn == true {
+            timeRemaining = nil
+            timer?.invalidate()
+            delegate?.timerStopped()
+        }
+    }
+} // End of Class
